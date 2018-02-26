8d02743e36bb92df965992e413880cd4f174c913
xJarlaxle <31731520+xJarlaxle@users.noreply.github.com>
2018-02-26 17:24:58 -0400

    Created Boundary Object
    Created a boundary object that destroys the player's shot when it leaves the boundary area.

---
+++ UnityCapstone/Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour {
+
+    /// <summary>
+    /// Detects when an object is leaving the boundary area and destroys that object
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerExit(Collider other) {
+        // Destroy everything that leaves the trigger
+        Destroy(other.gameObject);
+    }
+}
