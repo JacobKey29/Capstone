59f1812cd5a780e8248306a30aa36c48a55612b9
xJarlaxle <31731520+xJarlaxle@users.noreply.github.com>
2018-04-09 18:00:39 -0300

    Asteroid Explosion Working
    Game controller also added

--- UnityCapstone/Assets/Scripts/DestroyByContact.cs
+++ UnityCapstone/Assets/Scripts/DestroyByContact.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+    //reference to explosion visual effect
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
         {
             return;
         }
+        //asteroid explosion
+        Instantiate(explosion, transform.position, transform.rotation);
+        //player explosion, if hit by asteroid
+        if (other.tag == "Player") {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            //gameController.GameOver();
+        }
+
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
---
+++ UnityCapstone/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+
+    // Use this for initialization
+    void Start () {
+        SpawnWaves();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnWaves() {
+        //Randomly spawn the asteroid along the x plane
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        //give the asteroid rotation
+        Quaternion spawnRotation = Quaternion.identity;
+        //create
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+}
